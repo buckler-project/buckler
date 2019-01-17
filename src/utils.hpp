1909e3098c54ad683a92c5af29687a1b40bb67fd
akakou <s16003@tokyo.kosen-ac.jp>
2019-01-17 22:56:42 +0900

    [add] Core for repository.
    Add core for repository and test code.

--- src/utils.hpp
+++ src/utils.hpp
@@ -3,19 +3,92 @@
 #include <vector>
 #include <iostream>
 
+#include <boost/filesystem.hpp>
+#include <boost/foreach.hpp>
+#include <boost/range/iterator_range.hpp>
 
-template <typename T>
+
+#include "yaml-cpp/yaml.h"
+
+
+namespace buckler {
+template <class T>
 class IteratableObject {
 public:
-    std::vector<T> data;
+    std::vector<T> data = {};
     typename std::vector<T>::iterator itr;
     bool is_continue = false;
 
     IteratableObject() {}
 
-    void Start() {}
+    void Start() {
+        itr = data.begin();
+        is_continue = true;
+    }
     
-    T Next() {}
+    T Next() {
+        if (!is_continue) {
+            return *(data.end());
+        }
 
-    void Add(T object) {}
+        if (data.size() == 0) {
+            std::cerr 
+                << "failed to get element from vector " 
+                << "(vector has no elements)"
+                << std::endl;
+            
+            return *(data.begin());
+        }
+
+        typename std::vector<T>::iterator result = itr;
+
+        itr ++;
+
+        if(itr == data.end()) {
+            is_continue = false;
+        }
+
+        return *result;
+    }
+
+    void Add(T object) {
+        data.push_back(object);
+    }
+};
+
+
+template <class T>
+class Repository {
+public:
+    std::string parent_path;
+    std::string config_name;
+    IteratableObject<T> *list;
+
+    Repository() {}
+
+    Repository(IteratableObject<T> *list, std::string parent_path) {
+        this -> list = list;
+        this -> parent_path = parent_path;
+    }
+
+    virtual T Load(YAML::Node config, std::string path) {
+        std::cerr << "[err] not defined load." << std::endl;
+        return (T)0;
+    }
+
+    void LoadAll(){
+        namespace fs = boost::filesystem;
+
+        for (const auto& e : boost::make_iterator_range(fs::directory_iterator(parent_path), {})) {
+            if (fs::is_directory(e)) {
+                std::string path = e.path().string();
+                std::string config_path = path + "/" + config_name;
+
+                YAML::Node config = YAML::LoadFile(config_path);
+                T object = Load(config, path);
+                list->Add(object);
+            }
+        }
+    }
+};
 }
