213575a14e6f8660bdd87db3f9de2e2aabf34c61
akakou <s16003@tokyo.kosen-ac.jp>
2019-01-21 16:22:00 +0900

    [fix] Test code.
    Fix test code for `Buckler` and `SignaturesController`.

--- tests/src/signature.cc
+++ tests/src/signature.cc
@@ -75,6 +75,33 @@ TEST_F(SignaturesTest, PathList) {
     std::cout << std::endl;
 }
 
+TEST_F(SignaturesTest, Contoller) {
+    std::vector<std::string> vec = {
+        "./data/signatures/second",
+        "./data/signatures/first"
+    };
+
+    SignaturesController controller = SignaturesController();
+    controller.repository.parent_path = "./data/signatures";
+    controller.Load();
+
+    std::cout << std::endl;
+    std::cout << "-*-*-*-*- DATA -*-*-*-*-" << std::endl;
+    std::cout << std::endl;
+
+    controller.list.Start();
+    for(auto itr = vec.begin(); (itr != vec.end() && controller.list.is_continue) ; ++itr) {
+        Signature signatures = controller.list.Next();
+        std::cout << std::endl;
+        std::cout << "path: " << signatures.path << std::endl;
+        std::cout << "expected: " << *itr << std::endl;
+        std::cout << std::endl;
+        ASSERT_EQ(*itr, signatures.path);
+    }
+
+    std::cout << std::endl;
+}
+
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
